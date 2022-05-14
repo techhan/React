7594a5d29e54cc06f2d8d57bcf0f2d79f0cbfc7d
techhan <it_hs@naver.com>
2022-05-14 17:51:54 +0900

    react에서 배열 사용하기1 - 리스트 렌더링(조회)

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,12 +1,37 @@
-import './App.css';
-import DiaryEditor from './DiaryEditor';
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
+const dummyList = [
+  {
+    id: 1,
+    author: "레호",
+    content: "안녕1",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "홍길동",
+    content: "안녕2",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "유관순",
+    content: "안녕3",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+];
 function App() {
   return (
-  <div className="App">
-    <DiaryEditor/>
-  </div>
+    <div className="App">
+      <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
+    </div>
   );
-};
+}
 
 export default App;
--- simplediary/src/DiaryEditor.js
+++ simplediary/src/DiaryEditor.js
@@ -1,6 +1,11 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 const DiaryEditor = () => {
+
+    // useRef의 MutableRefObject는 HTML의 DOM 요소에 접근할 수 있도록 하는 기능
+    const authorInput = useRef();
+    const contentInput = useRef();
+    
 
     const [state, setState] = useState({
         author:"",
@@ -8,9 +13,6 @@ const DiaryEditor = () => {
         emotion:1,
         }
     );
-
-    // const [author,setAuthor] = useState("");
-    // const [content,setContent] = useState("");
 
     const handleChangeState = (e) => {
         console.log(e.target.name);
@@ -23,7 +25,19 @@ const DiaryEditor = () => {
     };
 
     const handleSubmit =(e) => {
-        console.log(state);
+        if(state.author.length < 1) {
+            //focus
+            authorInput.current.focus();
+            //useRef를 이용해 생성한 객체는 현재 가리키는 값을 current로 가져올 수 있음.
+            // 즉 authorInput.current는 input을 가리킨다.
+            return ;
+        }
+
+        if(state.content.length < 5) {
+            //focus
+            contentInput.current.focus();
+            return ;
+        }
         alert("저장 성공");
     }
 
@@ -33,11 +47,13 @@ const DiaryEditor = () => {
         <div>
             <input name="author" value={state.author} 
             onChange={handleChangeState}
+            ref={authorInput}
             />
         </div>
         <div>
             <textarea name="content" valeu={state.content} 
             onChange={handleChangeState}
+            ref={contentInput}
             />
         </div>
         <div>
